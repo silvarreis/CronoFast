9a80a8a4a22ba37313a60889dbf40e9e8112a502
Diogo-Oliveira02 <diogo.oss29@gmail.com>
2026-08-06 13:27:32 -0300

    Correção do erros

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,23 +45,7 @@
             <button type="button" id="nextStep">Continuar</button>
         </div>
     </form>
-    <div id="cardPaymentBrick_container"></div>
+    
     <p id="copy">&copy; 2025 - {{ $now }} | Dev: <a href="#">Diogo Oliveira</a></p>
     
 </x-guest-layout>
-
-
-    <form method="POST" id="form" action="{{ route('register') }}">
-        @csrf
-        <input type="text" name="name" id="name" placeholder="Nome">
-        <input type="email" name="email" id="email" placeholder="Email">
-        <input type="password" name="password" id="password" placeholder="Senha">
-        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Confirmar senha">
-        <button type="submit">Cadastrar</button>
-        <a href="/login">Voltar</a>
-    </form>
-
-    <p id="copy">&copy; 2025 - {{ $now }} | Diogo da Silva Oliveira</p>
-    
-</x-guest-layout>
-
